c919a00c95113f21cba702c811985247e2f644fe
glykeselpides-bit <glykeselpides@gmail.com>
2026-05-24 05:31:57 +0100

    i choose optimism

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,11 @@
 import { PrismaClient } from "../generated/prisma";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
