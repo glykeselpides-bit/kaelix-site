467f57224d62720b51a087dc86fdaa9b4c3fbf83
glykeselpides-bit <glykeselpides@gmail.com>
2026-05-25 01:30:12 +0100

    optimism is my testing strategy

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,9 @@ const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
 };
 
-export function getPrisma() {
-  const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
-  if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
-  }
-
-  return prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
